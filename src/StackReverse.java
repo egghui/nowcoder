a0ad3505ed741e8eded3bfdf96b00f016624c376
egghui <lidanhuide@sina.com>
2017-03-27 14:58:26 +0800

    数组解法 递归

--- src/StackReverse.java
+++ src/StackReverse.java
@@ -5,34 +5,16 @@ import java.util.*;
 
 public class StackReverse {
     public int[] reverseStack(int[] A, int n) {
-        Stack<Integer> stack = new Stack<Integer>();
         // write code here
-        for(int i = n-1; i == 0; i --) {
-            stack.push(A[i]);
+        if(A==null || n == 1) {
+            return A;
         }
-        reverse(stack);
-        for(int i=0;i<n;i++) {
-            A[i++]=stack.pop();
+        int i =  A[0];
+        for(int j = 0; j < n-1; j ++) {
+            A[j] = A[j+1];
         }
-        return  A;
-    }
-
-    public static int getAndRemoveLastElement(Stack<Integer> stack) {
-        int result = stack.pop();
-        if(stack.isEmpty()){
-            return result;
-        }else {
-            int last = getAndRemoveLastElement(stack);
-            stack.push(result);
-            return last;
-        }
-    }
-    public  static void reverse(Stack<Integer> stack) {
-        if(stack.isEmpty()) {
-            return;
-        }
-        int i = getAndRemoveLastElement(stack);
-        reverse(stack);
-        stack.push(i);
+        reverseStack(A,n-1);
+        A[n-1] = i;
+        return A;
     }
 }

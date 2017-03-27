857c26793bf34404942ff8cba15e811cd479d678
egghui <lidanhuide@sina.com>
2017-03-27 10:42:04 +0800

    ac

--- src/TwoStack.java
+++ src/TwoStack.java
@@ -6,38 +6,29 @@ import java.util.*;
 public class TwoStack {
     public int[] twoStack(int[] ope, int n) {
         // write code here
-        int[] res = {};
+        int[] res;
+        int count = 0;
         Stack<Integer> stackPush = new Stack<Integer>();
         Stack<Integer> stackPop = new Stack<Integer>();
         int i = 0;
-        int j = 0;
-        while(i < n) {
-            if(ope[i] != 0) {
-                spush(ope,n,stackPush,stackPop,i);
-            }else {
-                spop(stackPush,stackPop);
+        while (i < n) {
+            if (ope[i] != 0) {
+                stackPush.push(ope[i]);//一次性入栈
+            } else {
+                count++;
             }
+            i ++;
+        }
+        res = new int[count];
+        //stackPop此时为空 满足条件2
+        while (!stackPush.isEmpty()) {
+            stackPop.push(stackPush.pop());//条件1
+        }
+        i = 0;
+        while (count != 0) {
+            res[i++] = stackPop.pop();
+            count--;
         }
         return res;
-    }
-    public void spush(int[] ope, int n, Stack<Integer> stackPush,Stack<Integer> stackPop, int i) {
-        if (i==0||ope[i - 1] != 0) {
-            stackPush.push(ope[i]);
-        } else {
-            while (!stackPop.isEmpty()) {
-                stackPush.push(stackPop.pop());
-            }
-            stackPush.push(ope[i]);
-        }
-    }
-    public int spop(Stack<Integer> stackPush,Stack<Integer> stackPop) {
-        if(stackPop.empty()&&stackPush.empty()) {
-            throw new RuntimeException("Queue is empty");
-        }else if(stackPop.empty()) {
-            while (!stackPush.empty()) {
-                stackPop.push(stackPush.pop());
-            }
-        }
-        return stackPop.pop();
     }
 }

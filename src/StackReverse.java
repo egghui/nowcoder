dd1a05721c78555f45687f1971962b30ba083a8e
egghui <lidanhuide@sina.com>
2017-03-27 15:06:10 +0800

    数组解法 非递归 简单

--- src/StackReverse.java
+++ src/StackReverse.java
@@ -6,15 +6,13 @@ import java.util.*;
 public class StackReverse {
     public int[] reverseStack(int[] A, int n) {
         // write code here
-        if(A==null || n == 1) {
-            return A;
+        if(A==null)
+            return null;
+        for(int i=0;i*2<n;i++){
+            int tmp = A[i];
+            A[i] = A[n-1-i];
+            A[n-1-i] = tmp;
         }
-        int i =  A[0];
-        for(int j = 0; j < n-1; j ++) {
-            A[j] = A[j+1];
-        }
-        reverseStack(A,n-1);
-        A[n-1] = i;
         return A;
     }
 }

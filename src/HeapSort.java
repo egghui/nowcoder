ce20cd37b6be40a03f6aaea3bd19f86f9d0315c9
egghui <lidanhuide@sina.com>
2017-03-29 09:28:55 +0800

    2.7堆排序 牛客网友写法

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -4,38 +4,43 @@
 import java.util.*;
 
 public class HeapSort {
-    public int[] heapSort(int[] A, int n) {//最小堆排序完是递减数组 最大堆排序完为递增数组
+    public int[] heapSort(int[] A, int n) {
         // write code here
         for(int i=n/2; i>=0; i--){
-            fixDown(A,i,n);
+            heapAdjust(A,i,n);
         }
 
         for(int i=n-1;i>0;i--){
             swap(A,0,i);
-            fixDown(A,0,i);
+            heapAdjust(A,0,i);
         }
         return A;
     }
-    public void fixDown(int A[],int i, int n) {//题目要递增数组 故最大堆
-        int j = i*2 +1;
-        int tmp = A[i];
-        while(j < n) {
-            if(j+1<n && A[j+1]>A[j]) {//取两个孩子中较大者
-                j ++;
+
+    void heapAdjust(int[] A,int index,int length){
+        int childLeft;
+        int temp = A[index];
+        for( ;index*2+1 < length;index = childLeft){
+            childLeft = index*2+1;
+            if(childLeft !=length-1 && A[childLeft] < A[childLeft+1]){
+                childLeft++;
             }
-            if(A[j] <= tmp) {
-                break;//调整完毕
+            if(temp > A[childLeft]){
+                break;
+
             }
-            A[i] = A[j];//否则大者上浮
-            i = j;
-            j = i*2 +1;
+            else {
+                A[index] = A[childLeft];
+                index = childLeft;
+            }
         }
-        A[i] = tmp;//下沉到它的位置
+        A[index] = temp;
+
     }
 
-    public void swap(int A[],int i, int j) {
-        int tmp = A[i];
-        A[i] = A[j];
-        A[j] = tmp;
+    static void  swap(int[] A,int m,int n){
+        int temp = A[m];
+        A[m] = A[n];
+        A[n] = temp;
     }
 }
